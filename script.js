33e67131f9edea984f797ec2c881f5de46fadcaa
Smek-one <62439833+Smek-one@users.noreply.github.com>
2021-07-18 22:23:55 +0200

    Javascript rocks !

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let gamePlaying, roundScore, activePlayer, scores;
 
 
 
+
 //ROLL DICE
 document.getElementById('rollBtn').addEventListener('click', function() {
     if (gamePlaying) {
@@ -27,7 +28,6 @@ document.getElementById('rollBtn').addEventListener('click', function() {
 document.getElementById('holdBtn').addEventListener('click', function() {
     if (gamePlaying) {
         scores[activePlayer] += roundScore;
-
         document.querySelector('#globalScore-' + activePlayer).textContent = scores[activePlayer];
 
     }
@@ -46,14 +46,14 @@ document.getElementById('holdBtn').addEventListener('click', function() {
 
 //NEXT PLAYER
 function nextPlayer() {
-    activePlayer === 1 ? activePlayer = 2 : activePlayer = 1;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
     roundScore = 0;
 
+    document.getElementById('currentScore-0').textContent = '0';
     document.getElementById('currentScore-1').textContent = '0';
-    document.getElementById('currentScore-2').textContent = '0';
 
+    document.querySelector('#player-0-panel').classList.toggle('active');
     document.querySelector('#player-1-panel').classList.toggle('active');
-    document.querySelector('#player-2-panel').classList.toggle('active');
 
     document.querySelector('#dice').style.display = 'none';
 };
@@ -66,25 +66,25 @@ document.querySelector('#newGameBtn').addEventListener('click', (init));
 function init() {
 
     scores = [0, 0];
-    activePlayer = 1;
+    activePlayer = 0;
     roundScore = 0;
     gamePlaying = true;
 
 
     document.getElementById('dice').style.display = 'none';
 
+    document.getElementById('globalScore-0').textContent = '0';
     document.getElementById('globalScore-1').textContent = '0';
-    document.getElementById('globalScore-2').textContent = '0';
+    document.getElementById('currentScore-0').textContent = '0';
     document.getElementById('currentScore-1').textContent = '0';
-    document.getElementById('currentScore-2').textContent = '0';
+    document.querySelector('#name-0').classList.remove('winner');
     document.querySelector('#name-1').classList.remove('winner');
-    document.querySelector('#name-2').classList.remove('winner');
-    document.querySelector('#name-1').textContent = 'Player 1';
-    document.querySelector('#name-2').textContent = 'Player 2';
+    document.querySelector('#name-0').textContent = 'Player 1';
+    document.querySelector('#name-1').textContent = 'Player 2';
+    document.querySelector('#player-0-panel').classList.remove('active');
     document.querySelector('#player-1-panel').classList.remove('active');
-    document.querySelector('#player-2-panel').classList.remove('active');
+    document.querySelector('#player-0-panel').classList.remove('winner');
     document.querySelector('#player-1-panel').classList.remove('winner');
-    document.querySelector('#player-2-panel').classList.remove('winner');
-    document.querySelector('#player-1-panel').classList.add('active');
+    document.querySelector('#player-0-panel').classList.add('active');
 
 };
